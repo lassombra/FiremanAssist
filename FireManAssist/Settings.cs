73e8b9a0d2fb2c747b9c77e3fd30b7c3aca4abf0
Brian Engelhardt <lassombra@live.com>
2023-08-23 00:34:46 -0500

    Adds options to mod menu to allow users to select the assist level they want - defaults to protecting against over or under filling the boiler.
    Closes #11

---
+++ FireManAssist/Settings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityModManagerNet;
+
+namespace FireManAssist
+{
+    public enum WaterAssistMode
+    {
+        None,
+        No_Explosions,
+        Over_Under_Protection,
+        Full
+    }
+    public class Settings : UnityModManager.ModSettings, IDrawable
+    {
+        [Draw(DrawType.ToggleGroup)] public WaterAssistMode WaterMode = WaterAssistMode.Over_Under_Protection;
+
+        public void OnChange()
+        { }
+
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+    }
+}
