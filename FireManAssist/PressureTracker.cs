8d351c886f26a1bc5b844351b36644130ee7da93
Brian Engelhardt <lassombra@live.com>
2023-08-24 20:31:17 -0500

    Add Pressure Trend calculator that uses a simple rolling array (queue) to report trend, requires both short and longterm trends to be a certain way in order to report a trend.

---
+++ FireManAssist/PressureTracker.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireManAssist
+{
+    public enum Trend
+    {
+        Rising,
+        Falling,
+        Steady
+    }
+    public class PressureTracker
+    {
+        private float[] _pressureHistory = new float[5];
+        private int _historyIndex = 0;
+        public Trend UpdateAndCheckTrend(float pressure)
+        {
+            var lastPressure = GetLastPressure();
+            _pressureHistory[_historyIndex] = pressure;
+            _historyIndex++;
+            if (_historyIndex == 5)
+            {
+                _historyIndex = 0;
+            }
+            var oldestPressure = getOldestPresure();
+            if (oldestPressure == 0f)
+            {
+                return Trend.Steady;
+            }
+            return GetTrend(oldestPressure, lastPressure, pressure);
+        }
+        private float GetLastPressure()
+        {
+            var lastPressureIndex = _historyIndex - 1;
+            if (lastPressureIndex < 0)
+            {
+                lastPressureIndex = 4;
+            }
+            return _pressureHistory[lastPressureIndex];
+        }
+        private float getOldestPresure()
+        {
+            return _pressureHistory[_historyIndex];
+        }
+        private Trend GetTrend(float oldestPressure, float lastPressure, float pressure)
+        {
+            var immediateTrend = GetTrendFromStates(lastPressure, pressure);
+            var longTermTrend = GetTrendFromStates(oldestPressure, pressure);
+            if (immediateTrend == longTermTrend)
+            {
+                return immediateTrend;
+            }
+            else
+            {
+                return Trend.Steady;
+            }
+        }
+
+        private Trend GetTrendFromStates(float lastPressure, float pressure)
+        {
+            float difference = pressure - lastPressure;
+            return DifferenceToTrend(difference);
+        }
+
+        private Trend DifferenceToTrend(float difference)
+        {
+            if (difference > 0)
+            {
+                return Trend.Rising;
+            }
+            else if (difference < 0)
+            {
+                return Trend.Falling;
+            }
+            else
+            {
+                return Trend.Steady;
+            }
+        }
+    }
+}
---
+++ FiremanAssistantTests/PressureTrackerTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FireManAssist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireManAssist.Tests
+{
+    [TestClass()]
+    public class PressureTrackerTests
+    {
+        [TestMethod()]
+        public void IntialTrendIsNeutralUntil5ReceivedTest()
+        {
+            var tracker = new PressureTracker();
+            Assert.AreEqual(Trend.Steady, tracker.UpdateAndCheckTrend(13f));
+            Assert.AreEqual(Trend.Steady, tracker.UpdateAndCheckTrend(13f));
+            Assert.AreEqual(Trend.Steady, tracker.UpdateAndCheckTrend(13f));
+            Assert.AreEqual(Trend.Steady, tracker.UpdateAndCheckTrend(13f));
+            Assert.AreNotEqual(Trend.Steady, tracker.UpdateAndCheckTrend(14f));
+        }
+        [TestMethod()]
+        public void ImmediateTrendAndLongTermTrendDifferenceIsNeutralTest()
+        {
+            var tracker = new PressureTracker();
+            tracker.UpdateAndCheckTrend(12f);
+            tracker.UpdateAndCheckTrend(13f);
+            tracker.UpdateAndCheckTrend(14f);
+            tracker.UpdateAndCheckTrend(13.5f);
+            Assert.AreEqual(Trend.Steady, tracker.UpdateAndCheckTrend(13f));
+
+            Assert.AreEqual(Trend.Steady, tracker.UpdateAndCheckTrend(13f));
+        }
+
+        [TestMethod()]
+        public void ImmediateAndLongTermRiseTest()
+        {
+            var tracker = new PressureTracker();
+            tracker.UpdateAndCheckTrend(12f);
+            tracker.UpdateAndCheckTrend(12.5f);
+            tracker.UpdateAndCheckTrend(13f);
+            tracker.UpdateAndCheckTrend(13.5f);
+            Assert.AreEqual(Trend.Rising, tracker.UpdateAndCheckTrend(14f));
+            Assert.AreEqual(Trend.Steady, tracker.UpdateAndCheckTrend(13.5f));
+        }
+
+        [TestMethod()]
+        public void ImmediateAndLongTermFallTest()
+        {
+            var tracker = new PressureTracker();
+            tracker.UpdateAndCheckTrend(14f);
+            tracker.UpdateAndCheckTrend(13.8f);
+            tracker.UpdateAndCheckTrend(13.7f);
+            tracker.UpdateAndCheckTrend(13.2f);
+            Assert.AreEqual(Trend.Falling, tracker.UpdateAndCheckTrend(13f));
+            Assert.AreEqual(Trend.Steady, tracker.UpdateAndCheckTrend(13.1f));
+        }
+
+        [TestMethod()]
+        public void ImmedateEqualIsSteadyTest()
+        {
+            var tracker = new PressureTracker();
+            tracker.UpdateAndCheckTrend(14f);
+            tracker.UpdateAndCheckTrend(13.8f);
+            tracker.UpdateAndCheckTrend(13.7f);
+            tracker.UpdateAndCheckTrend(13.2f);
+            Assert.AreEqual(Trend.Steady, tracker.UpdateAndCheckTrend(13.2f));
+            Assert.AreEqual(Trend.Falling, tracker.UpdateAndCheckTrend(13f));
+            Assert.AreEqual(Trend.Steady, tracker.UpdateAndCheckTrend(13.7f));
+        }
+    }
+}
